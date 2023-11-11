5f5a24f916f08b688d579bb03d052867bf365059
Ahmet Enes Semerci <ahmetenessemerci@hotmail.com>
2023-11-11 23:09:36 +0300

    feat: TaxCalculator Program

---
+++ TaxCalculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TaxCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double taxPercentage = 0.18;
+        System.err.print("Price Without Tax: ");
+        double price = scanner.nextDouble();
+        double priceWithTax = price * (1+taxPercentage);
+        System.err.println();
+    System.err.printf("KDV'siz Fiyat = %.2f \n",price);
+    System.err.printf("KDV'li Fiyat = %.2f \n",priceWithTax);
+    System.err.printf("KDV tutarı = %.2f \n",(price*taxPercentage));
+ }
+    
+}
